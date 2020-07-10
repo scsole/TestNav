b5da82916ebce5ff82f4a28a1f8d12bbd836e62c
Sebastian Sole <sebastian.c.sole@gmail.com>
2020-07-10 21:19:09 +1200

    Add some comments.

--- app/src/main/java/com/example/testnav/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/example/testnav/ui/notifications/NotificationsViewModel.kt
@@ -4,10 +4,17 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+/**
+ * Notifications view model comment.
+ */
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
     }
+
+    /**
+     * Live data comment.
+     */
     val text: LiveData<String> = _text
 }
