ec24015f50c71c36e6a8bfdd882b2ea0ce56e742
Sebastian Sole <sebastian.c.sole@gmail.com>
2020-07-17 00:31:48 +1200

    Add instrumented test

--- app/src/main/java/com/example/testnav/ExampleUtils.kt
+++
@@ -1,7 +0,0 @@
-package com.example.testnav
-
-class ExampleUtils {
-    companion object{
-        fun square(x: Int) = x * x
-    }
-}
---
+++ app/src/main/java/com/example/testnav/Utils.kt
@@ -0,0 +1,8 @@
+package com.example.testnav
+
+class Utils {
+    companion object {
+        fun square(x: Int) = x * x
+        fun double(x: Int) = 2 * x
+    }
+}
